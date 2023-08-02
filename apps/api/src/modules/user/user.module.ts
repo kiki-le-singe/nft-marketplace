d0fb4da18ef465ade1320feedb5cfcb3cc53a645
anthony <aalbertini95@gmail.com>
2023-08-02 15:46:43 +0200

    set PrismaService as provider

--- apps/api/src/modules/user/user.module.ts
+++ apps/api/src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from 'src/controllers/user/user.controller';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 import { UserService } from 'src/services/user/user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PrismaService],
 })
 export class UserModule {}
