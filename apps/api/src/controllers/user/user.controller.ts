0c697146619595868a45ef61882fa0cb95914b03
anthony <aalbertini95@gmail.com>
2023-08-06 18:40:48 +0200

    rename user api route

--- apps/api/src/controllers/user/user.controller.ts
+++ apps/api/src/controllers/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from 'src/services/user/user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
