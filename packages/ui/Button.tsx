8cc5f42f356a1668f2bf9b4c11fa4523597008b9
anthony <aalbertini95@gmail.com>
2023-08-29 11:08:03 +0200

    Implement Storybook

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,7 +1,30 @@
 "use client";
 
-import * as React from "react";
+// import * as React from "react";
 
-export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
+// export const Button = () => {
+//   return <button onClick={() => alert("boop")}>Boop</button>;
+// };
+
+interface Props {
+  primary?: boolean;
+  size?: "small" | "large";
+  label?: string;
+}
+
+export const Button = ({
+  primary = false,
+  label = "Boop",
+  size = "small",
+}: Props) => {
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? "red" : "blue",
+        fontSize: size === "large" ? "24px" : "14px",
+      }}
+    >
+      {label}
+    </button>
+  );
 };
