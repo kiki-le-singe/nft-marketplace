0e0e3f54851fa6e168e8aee0987c919f7a8e020b
anthony <aalbertini95@gmail.com>
2023-11-06 14:20:25 +0100

    add images next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.animaapp.com",
+        port: "",
+        pathname: "/projects/**/img/**",
+      },
+    ],
+  },
 };
