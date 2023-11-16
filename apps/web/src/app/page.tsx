c7d4a9e03459c5c8fc9be1a553f2118625f15de0
anthony <aalbertini95@gmail.com>
2023-11-16 18:17:50 +0100

    Add Category Card grid

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { NFTCard, AvatarName } from "ui";
+import { NFTCard, AvatarName, CategoryCard } from "ui";
 
 export default function Page() {
   return (
@@ -22,6 +22,27 @@ export default function Page() {
           name="Mr Fox"
           theme="big"
         />
+      </div>
+
+      <div className="flex w-full justify-center py-10 md:py-14 lg:py-20 xxs:px-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-7 lg:gap-7">
+          <CategoryCard
+            title="Art"
+            image="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-55@2x.png"
+          />
+          <CategoryCard
+            title="Art"
+            image="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-55@2x.png"
+          />
+          <CategoryCard
+            title="Art"
+            image="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-55@2x.png"
+          />
+          <CategoryCard
+            title="Art"
+            image="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-55@2x.png"
+          />
+        </div>
       </div>
     </div>
   );
