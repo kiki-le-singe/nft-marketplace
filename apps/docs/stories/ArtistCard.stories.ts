878d1afa6405b3323607399156ecb549ae7094aa
anthony <aalbertini95@gmail.com>
2023-11-30 15:39:01 +0100

    prettier

--- apps/docs/stories/ArtistCard.stories.ts
+++ apps/docs/stories/ArtistCard.stories.ts
@@ -25,10 +25,9 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     creator: {
-        avatar:
-            "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
-        name: "Mr Fox",
-    }
+      avatar:
+        "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
+      name: "Mr Fox",
+    },
   },
 };
-
