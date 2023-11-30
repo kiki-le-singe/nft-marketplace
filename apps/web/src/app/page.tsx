fdf810dc246723f2247dab0918d0b23306fec1e0
anthony <aalbertini95@gmail.com>
2023-11-30 14:28:36 +0100

    [WIP] Implement ArtistCard grid

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { AvatarName, CategoryCard, GridCategoryCard, GridNFTCard } from "ui";
+import { ArtistCard, AvatarName, GridCategoryCard, GridNFTCard } from "ui";
 
 export default function Page() {
   return (
@@ -18,6 +18,53 @@ export default function Page() {
       </div>
 
       <GridCategoryCard />
+
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-[30px]">
+          <ArtistCard
+            creator={{
+              avatar:
+                "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
+              name: "Mr Fox",
+            }}
+          />
+          <ArtistCard
+            creator={{
+              avatar:
+                "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
+              name: "Mr Fox",
+            }}
+          />
+          <ArtistCard
+            creator={{
+              avatar:
+                "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
+              name: "Mr Fox",
+            }}
+          />
+          <ArtistCard
+            creator={{
+              avatar:
+                "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
+              name: "Mr Fox",
+            }}
+          />
+          <ArtistCard
+            creator={{
+              avatar:
+                "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
+              name: "Mr Fox",
+            }}
+          />
+          <ArtistCard
+            creator={{
+              avatar:
+                "https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png",
+              name: "Mr Fox",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
