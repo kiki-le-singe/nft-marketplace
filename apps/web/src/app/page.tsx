60007d4a58b441d74aaf8ca9cc70d19372191a07
anthony <aalbertini95@gmail.com>
2023-12-19 15:11:22 +0100

    remove unused component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,8 +3,6 @@ import { AvatarName, GridArtistCard, GridCategoryCard, GridNFTCard } from "ui";
 export default function Page() {
   return (
     <div className="flex flex-col xxs:px-2 xs:px-5 sm:px-5 lg:px-0 gap-y-10 md:gap-y-14 lg:gap-y-20">
-      <GridNFTCard />
-
       <div>
         <AvatarName
           avatar="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-98@2x.png"
