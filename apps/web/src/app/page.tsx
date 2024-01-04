0ac28853d35370d77badb155c1d5a2d74ec93497
anthony <aalbertini95@gmail.com>
2024-01-04 10:17:45 +0100

    remove unused components

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,4 @@
 import {
-  AvatarName,
   GridArtistCard,
   GridCategoryCard,
   GridNFTCard,
@@ -11,18 +10,6 @@ export default function Page() {
   return (
     <div className="flex flex-col gap-y-10 md:gap-y-14 lg:gap-y-20 w-full py-10">
       <div className="flex flex-col xxs:px-2 xs:px-5 sm:px-10 lg:px-[5vw] xl:px-[15vw] 2xl:px-[15vw] gap-y-10 md:gap-y-14 lg:gap-y-20 w-full">
-        <div>
-          <AvatarName
-            avatar="http://localhost:3002/images/cat.png"
-            name="Mr Fox"
-          />
-          <AvatarName
-            avatar="http://localhost:3002/images/dog.png"
-            name="Mr Fox"
-            theme="big"
-          />
-        </div>
-
         <GridArtistCard />
         <GridCategoryCard />
         <GridNFTCard />
