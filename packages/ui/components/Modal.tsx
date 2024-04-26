cae87220d2682bc946d0eb57e61a48e0802b38cb
anthony <aalbertini95@gmail.com>
2024-04-26 16:48:11 +0200

    [styles] make modal styles responsive

--- packages/ui/components/Modal.tsx
+++ packages/ui/components/Modal.tsx
@@ -23,20 +23,22 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
-      <dialog
-        ref={dialogRef}
-        className="relative w-4/5 max-w-[500px] h-auto max-h-[500px] border-none rounded-xl bg-white p-5 flex items-center justify-center text-6xl font-medium"
-        onClose={onDismiss}
-      >
-        {children}
-        <button
-          onClick={onDismiss}
-          className="absolute top-2.5 right-2.5 w-12 h-12 bg-transparent border-none rounded-full cursor-pointer flex items-center justify-center font-medium text-2xl hover:bg-[#eee] text-black"
-        >
-          x
-        </button>
-      </dialog>
-    </div>
+    <dialog
+      ref={dialogRef}
+      className="w-full h-full max-w-full max-h-full m-0 bg-black bg-opacity-60"
+    >
+      <div className="w-full h-full">
+        <div className="absolute inset-0 xxs:m-5 xs:m-8 sm:m-14 lg:m-[8vw] xl:m-[16vw] 2xl:m-[16vw] flex justify-center items-center border border-purple-transparent rounded-xl bg-black">
+          {children}
+
+          <button
+            onClick={onDismiss}
+            className="absolute top-4 right-4 w-12 h-12 rounded-full cursor-pointer flex items-center justify-center font-medium text-2xl border-2 border-purple lg:hover:bg-purple active:bg-purple text-white custom-animation-scale"
+          >
+            x
+          </button>
+        </div>
+      </div>
+    </dialog>
   );
 }
