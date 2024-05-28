2a6271c90dea72ecef4ba17bd1ce39874d2eb68b
anthony <aalbertini95@gmail.com>
2024-05-28 14:50:49 +0200

    [SEO] add generated sitemap

---
+++ apps/web/src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "http://localhost:3000",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "http://localhost:3000/artist",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
